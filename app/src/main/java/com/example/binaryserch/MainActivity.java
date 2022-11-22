1c36bd3b79fdd4516ce0aef7fda6eb42a4dfb6fd
Nalanihv <nalanihevi@gmail.com>
2022-11-22 09:35:27 -0600

    comentarios

--- app/src/main/java/com/example/binaryserch/MainActivity.java
+++ app/src/main/java/com/example/binaryserch/MainActivity.java
@@ -11,11 +11,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* que hace
+        /* BIG O
+
+        que hace:
         La búsqueda binaria es un algoritmo eficiente para encontrar
         un elemento en una lista ordenada de elementos. Funciona al dividir
         repetidamente a la mitad la porción de la lista que podría contener al elemento,
-         hasta reducir las ubicaciones posibles a solo una.*/
+         hasta reducir las ubicaciones posibles a solo una.
+
+         Historia.
+         En 1962, Hermann Bottenbruch presentó en ALGOL 60 una implementación del algoritmo
+         de búsqueda binaria en el cual colocaba la comparación de igualdad en el final del algoritmo,
+          incrementando el número promedio de iteraciones por uno, pero reduciendo a uno el número de comparaciones por iteración.
+
+
+          algoritmo de búsqueda es un conjunto de instrucciones que están diseñadas para localizar un elemento con
+          ciertas propiedades dentro de una estructura de datos;
+
+          ventajas. Se puede aplicar tanto a datos en listas lineales como en árboles binarios de búsqueda.
+          método eficiente siempre que el vector esté ordenado.
+         */
 
     }
 
