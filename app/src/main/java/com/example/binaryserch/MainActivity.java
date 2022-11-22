7a6605c21c1e58b693c889a346d21176f5397378
Nalanihv <nalanihevi@gmail.com>
2022-11-22 09:27:29 -0600

    Initial commit

---
+++ app/src/main/java/com/example/binaryserch/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.binaryserch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* que hace
+        La búsqueda binaria es un algoritmo eficiente para encontrar
+        un elemento en una lista ordenada de elementos. Funciona al dividir
+        repetidamente a la mitad la porción de la lista que podría contener al elemento,
+         hasta reducir las ubicaciones posibles a solo una.*/
+
+    }
+
+}
